0df218912c8efbabe50256e8c687be9f47f579f8
Wojciech Walczyszyn <wojciech.walczyszyn@sap.com>
2021-06-10 16:09:35 +0200

    Additional styling and configuration options.

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,34 @@ import pjson from "../package.json";
 
 class CardConfig {
   entity?: string;
-  off_state_behaviour?: string; // progress, icon, hide
+  //off_state_behaviour?: string; // progress, icon, hide
   show?: ShowConfig;
   colors?: ColorConfig;
   icon?: string;
-  icon_off?: string;
   unit?: string;
   attribute?: string;
   value?: number | string;
   min?: number;
   max?: number;
   string_value?: string;
-  string_value_off?: string;
-  stroke_size?: string;
-  stroke_size_off?: string;
+  stroke_size?: number | string;
   size?: number;
   font_size?: number;
   icon_size?: number;
 }
 
 class ColorConfig {
-  progress_bar?: string;
-  track_bar?: string;
+  progress?: string;
+  track?: string;
   text?: string;
-  text_off?: string;
   icon?: string;
-  icon_off?: string;
 }
 
 class ShowConfig {
   value?: boolean;
   icon?: boolean;
-  progress_bar?: boolean;
-  track_bar?: boolean;
+  progress?: boolean;
+  track?: boolean;
   background?: boolean;
 }
 
@@ -89,9 +84,9 @@ class CircleProgressCard extends LitElement {
 
       numericValue = value;
     } else {
-      const parsedValue = parseInt(value);
+      const parsedValue = parseFloat(value);
 
-      if (parsedValue) {
+      if (parsedValue != null) {
         if (parsedValue > 0 && parsedValue < 1 && !min && !max && unit == '%') {
           min = 0;
           max = 1;
@@ -104,81 +99,54 @@ class CircleProgressCard extends LitElement {
     min = min ?? 0;
     max = max ?? 100;
 
-    // const displayedProgress = numericValue ? (numericValue - min) / (max - min) * 100 : undefined;
-    const progress = numericValue ? (numericValue - min) / (max - min) : 1;
-    const hideUnit = !numericValue || valueString;
-    const adjustedValue = numericValue ? (unit == '%' ? Math.round((numericValue - min) / (max - min) * 100) : Math.round(numericValue)) : undefined;
-    valueString = valueString ? `${valueString}` : adjustedValue ? `${adjustedValue}` : `${value}`;
+    const progress = numericValue != null ? (numericValue - min) / (max - min) : 1;
+    const hideUnit = numericValue == null || valueString;
+    const adjustedValue = numericValue != null ? (unit == '%' ? Math.round((numericValue - min) / (max - min) * 100) : Math.round(numericValue)) : undefined;
+    valueString = valueString ? `${valueString}` : adjustedValue != null ? `${adjustedValue}` : `${value}`;
 
-    const valueStringOff = this._getTemplateOrValue(this._state, this._config.string_value_off) ?? valueString;
+    const icon = this._getTemplateOrValue(this._state, this._config.icon) ?? this._state.attributes.icon;
 
-    //temp
-    const off_status = 'off';
+    const size = this._getTemplateOrValue(this._state, this._config.size) ?? 100;
+    const strokeSize = this._getTemplateOrValue(this._state, this._config.stroke_size) ?? 4;
+    const iconSize = this._getTemplateOrValue(this._state, this._config.icon_size) ?? 46;
+    const fontSize = this._getTemplateOrValue(this._state, this._config.font_size) ?? 16;
 
-    const state = 'on';//this._state.state === off_status ? 'off' : 'on animate-progress';
-    const radius = 23;
+    const progressColor = this._getTemplateOrValue(this._state, this._config.colors?.progress) ?? "var(--paper-item-icon-color, #44739e)";
+    const trackColor = this._getTemplateOrValue(this._state, this._config.colors?.track) ?? "var(--divider-color, #e8e8e8)";
+    const textColor = this._getTemplateOrValue(this._state, this._config.colors?.text) ?? "var(--primary-text-color)";
+    const iconColor = this._getTemplateOrValue(this._state, this._config.colors?.icon) ?? "var(--primary-text-color)";
+
+    const radius = Math.floor(25 - (strokeSize / 2));
     const circumference = radius * 2 * Math.PI;
-    // const colorTemp = 90 - (entity.attributes.color_temp - entity.attributes.min_mireds) / (entity.attributes.max_mireds - entity.attributes.min_mireds) * 50;
+
+    const showIcon = (this._getTemplateOrValue(this._state, this._config.show?.icon) ?? !numericValue) && icon;
+    const showValue = this._getTemplateOrValue(this._state, this._config.show?.value) ?? !showIcon;
+    const showTrack = this._getTemplateOrValue(this._state, this._config.show?.track) ?? true;
+    const showProgress = this._getTemplateOrValue(this._state, this._config.show?.progress) ?? true;
+    const showBackground = this._getTemplateOrValue(this._state, this._config.show?.background) ?? true;
 
     return html`
-    <ha-card>
-      <div style="--stroke-offset: ${circumference - progress * circumference}">
-      <style>
-        svg {
-          width: 100%;
-          top: 0;
-          left: 0;
-          //position: absolute;
-        }
-      </style>
-      <svg viewBox="0 0 50 50"">
-        <style>
-          circle {
-            transform: rotate(-90deg);
-            transform-origin: 50% 50%;
-            stroke-linecap: round;
-            transition: stroke-dashoffset 0.5s ease-in-out;
-            stroke-dasharray: ${circumference};
-            stroke-dashoffset: var(--stroke-offset);
-          }
-          circle.off {
-            stroke: var(--card-icon-color-off, var(--disabled-text-color));
-          }
-          circle.on {
-            stroke: var(--card-icon-color-on, var(--paper-item-icon-color, #44739e));
-          }
-          path.off,
-          text.off,
-          text.off tspan{
-            fill: var(--card-icon-color-off, var(--disabled-text-color));
-          }
-          path.on,
-          text.on,
-          text.on tspan {
-            fill: var(--card-icon-color-on, var(--primary-text-color));
-          }
-          text {
-            font-size: 16px;
-          }
-          tspan {
-            font-size: 12px;
-            alignment-baseline: central;
-          }
-          @keyframes animate-progress {
-            0% {
-              stroke-dashoffset: ${circumference};
-            }
-            100% {
-              stroke-dashoffset: var(--stroke-offset);
-            }
-          }
-          circle.animate-progress {
-            animation: animate-progress 0.8s;
-          }
-        </style>
-        <circle class="${state}" cx="25" cy="25" r="${radius}" stroke-width="2" fill="none" />
-        <text class="${state}" x="50%" y="54%" text-anchor="middle" alignment-baseline="middle" dominant-baseline="middle">${valueString}<tspan style="${hideUnit ? 'display: none;' : ''}">${unit}</tspan></text>
-      </svg>
+    <ha-card style="${showBackground ? '' : 'background: none; box-shadow: none;'}">
+      <div class="wrapper" style="
+      --stroke-circumference: ${circumference}; 
+      --stroke-offset: ${circumference - progress * circumference}; 
+      --stroke-size: ${strokeSize}; 
+      --progress-color: ${progressColor}; 
+      --track-color: ${trackColor}; 
+      --text-color: ${textColor}; 
+      --icon-color: ${iconColor};
+      --size: ${size}%;
+      --font-size: ${fontSize}px;">
+
+        <svg viewBox="0 0 50 50"">
+          <circle class="track-bar" cx="25" cy="25" r="${radius}" fill="none" style="${showTrack ? '' : 'display: none;'}"/>
+          <circle class="progress-bar animate-progress" cx="25" cy="25" r="${radius}" fill="none" style="${showProgress ? '' : 'display: none;'}"/>
+          <text x="50%" y="54%" text-anchor="middle" alignment-baseline="middle" dominant-baseline="middle" style="${showValue ? '' : 'display: none;'}">
+            ${valueString}<tspan style="${hideUnit ? 'display: none;' : ''}">${unit}</tspan>
+          </text>
+        </svg>
+        ${showIcon ? html`
+        <ha-icon id="progress-icon" icon="${icon}" style="color: ${iconColor}; --mdc-icon-size: ${iconSize}%;"></ha-icon>` : ''}
       </div>
     </ha-card>
     `;
@@ -203,11 +171,6 @@ class CircleProgressCard extends LitElement {
     }
   }
 
-  // private _objectEvalTemplate(state: any | undefined, obj: any | undefined): any {
-  //   const objClone = copy(obj);
-  //   return this._getTemplateOrValue(state, objClone);
-  // }
-
   private _getTemplateOrValue(state: any | undefined, value: any | undefined): any | undefined {
     if (['number', 'boolean'].includes(typeof value)) return value;
     if (!value) return value;
@@ -227,7 +190,56 @@ class CircleProgressCard extends LitElement {
 
   static get styles() {
     return css`
-      
+      .wrapper {
+        margin: calc((100% - var(--size)) / 2);
+      }
+      circle {
+        stroke-width: var(--stroke-size);
+      }
+      circle.track-bar {
+        stroke: var(--track-color);
+      }
+      circle.progress-bar {
+        stroke: var(--progress-color);
+        transform: rotate(-90deg);
+        transform-origin: 50% 50%;
+        stroke-linecap: round;
+        transition: stroke-dashoffset 0.5s ease-in-out;
+        stroke-dasharray: var(--stroke-circumference);
+        stroke-dashoffset: var(--stroke-offset);
+      }
+      path,
+      text,
+      text tspan {
+        fill: var(--text-color);
+      }
+      text {
+        font-size: var(--font-size);
+      }
+      tspan {
+        font-size: calc(0.75 * var(--font-size));
+        alignment-baseline: central;
+      }
+      @keyframes animate-progress {
+        0% {
+          stroke-dashoffset: var(--stroke-circumference);
+        }
+        100% {
+          stroke-dashoffset: var(--stroke-offset);
+        }
+      }
+      circle.animate-progress {
+        animation: animate-progress 0.8s;
+      }
+      ha-icon#progress-icon {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: calc(50% - var(--mdc-icon-size) / 2);
+        left: calc(50% - var(--mdc-icon-size) / 2);
+        right: auto;
+        bottom: auto;
+      }
     `;
   }
 }
